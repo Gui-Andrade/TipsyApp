54f64213c0f1172f6587d4233e29836bf5d1a3db
Gui Andrade <guikonetic@gmail.com>
2018-05-03 16:54:06 +0100

    Implementing About module/page

---
+++ src/components/About.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Header from './Header';
+
+class About extends React.Component{
+
+  render(){
+
+    return(
+      <main>
+        <Header tagline="Always pay and tip right" />
+
+        <div id="about" className="internal">
+
+          <h2>About <span>[ this magnificent app ]</span></h2>
+
+          <p>
+            Tipsy is a free app to help you calculate the right amount to pay/tip when you're out and about.
+          </p>
+          <p>
+            Especially at those moments when you can't quite be sure to get your maths right because you're tipsy*.
+          </p>
+          <blockquote cite="http://guiandrade.com">* See what I've done there? ;) <span>Gui Andrade</span></blockquote>
+
+        </div>
+      </main>
+    )
+  }
+}
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -5,14 +5,15 @@ import { BrowserRouter, Match, Miss } from 'react-router';
 import './css/style.css';
 
 import App from './components/App';
-
+import About from './components/About';
 import NotFound from './components/NotFound';
 
 const Root = () => {
   return (
     <BrowserRouter>
       <div>
-        <Match exactly pattern="/side-projects/tipsy/" component={App} />
+        <Match exactly pattern="/" component={App} />
+        <Match exactly pattern="/about/" component={About} />
         <Miss component={NotFound} />
       </div>
     </BrowserRouter>
